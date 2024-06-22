247caf8c439c6cdc4fad72dc49d2202e79f96967
hagiskawww <v.zhurabetmen@gmail.com>
2024-06-22 16:13:47 +0300

    Add JSlesson1

---
+++ JSlesson1/main.js
@@ -0,0 +1,9 @@
+const firstName = 'Vladislav'
+const lastName = 'Zhuravitsky'
+let age = 18
+let country = 'Israel'
+let city = 'Ashdod'
+
+console.log(firstName, lastName)
+console.log(age, 'y.o')
+console.log(country, city)
