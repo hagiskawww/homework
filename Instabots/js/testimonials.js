33f46c30c804c7323eb23e857e30b08dbbf6c283
hagiskawww <v.zhurabetmen@gmail.com>
2024-10-08 01:16:17 +0300

    Delete Instabots directory

--- Instabots/js/testimonials.js
+++
@@ -1,9 +0,0 @@
-var swiper = new Swiper(".mySwiper", {
-    slidesPerView: 2,
-    spaceBetween: 15,
-    freeMode: true,
-    navigation: {
-        nextEl: ".swiper-button-next",
-        prevEl: ".swiper-button-prev",
-    },
-});
