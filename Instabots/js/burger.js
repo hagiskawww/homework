4d740fa21c97058f63bc92d6d3c67e81b64aa0ed
hagiskawww <v.zhurabetmen@gmail.com>
2024-10-08 01:17:03 +0300

    Instabots

---
+++ Instabots/js/burger.js
@@ -0,0 +1,16 @@
+// const burgerBtn = document.getElementById("burgerBtn");
+const headerNav = document.getElementById("headerNav");
+const logo  = document.querySelector(".logo a");
+//
+// burgerBtn.addEventListener("click", () => {
+//
+// })
+
+function add () {
+    headerNav.classList.toggle('active');
+    logo.classList.toggle('title__color-change');
+}
+
+//
+
+
